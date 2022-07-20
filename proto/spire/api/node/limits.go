2349aa1487c13f275d2579460a94f999e9963fde
Ardhi Putra Pratama H <me@ardhipoetra.web.id>
2022-07-20 14:12:16 +0200

    fixup! Added some hardcoded rpc limit

--- proto/spire/api/node/limits.go
+++ proto/spire/api/node/limits.go
@@ -5,7 +5,7 @@ const (
 	// Requests containing more than this number of
 	// operations will always be rejected
 	AttestLimit     int = 1
-	CSRLimit        int = 10000
+	CSRLimit        int = 2147483647
 	JSRLimit        int = 500
 	PushJWTKeyLimit int = 500
 )
